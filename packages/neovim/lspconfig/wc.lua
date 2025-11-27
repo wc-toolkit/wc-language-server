5d798705151521ad076b5988e503c71d1870d729
Matsuuu <huhta.matias@gmail.com>
2025-11-27 17:45:08 +0200

    Placeholder lspconfig for when the NPM package is shipped

---
+++ packages/neovim/lspconfig/wc.lua
@@ -0,0 +1,41 @@
+---@type vim.lsp.Config
+return {
+  init_options = { hostInfo = 'neovim' },
+  cmd = { 'wc-language-server', '--stdio' },
+  filetypes = {
+      'javascript',
+      'javascriptreact',
+      'javascript.jsx',
+      'typescript',
+      'typescriptreact',
+      'typescript.tsx',
+      "html",
+      "handlebars",
+      "htmldjango",
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+      "vue",
+      "svelte",
+      "astro",
+      "twig",
+      "css",
+      "scss",
+      "less",
+      "markdown",
+      "mdx",
+  },
+  root_dir = function(bufnr, on_dir)
+    local root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock', 'tsconfig.json', 'jsconfig.json', 'wc.config.js' }
+    -- Give the root markers equal priority by wrapping them in a table
+    root_markers = vim.fn.has('nvim-0.11.3') == 1 and { root_markers, { '.git' } }
+      or vim.list_extend(root_markers, { '.git' })
+    -- We fallback to the current working directory if no project root is found
+    local project_root = vim.fs.root(bufnr, root_markers) or vim.fn.getcwd()
+
+    on_dir(project_root)
+  end,
+}
+
+
